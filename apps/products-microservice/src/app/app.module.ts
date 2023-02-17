7fd0611c2f6ad5e3f46bc4eb9029a7ab470a9de4
Lekan Adetunmbi <lothbroch@gmail.com>
2023-02-17 16:09:46 +0100

    feat(product-microservice): added mongodb database integration

--- apps/products-microservice/src/app/app.module.ts
+++ apps/products-microservice/src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
+import { ClassSerializerInterceptor, Module } from "@nestjs/common";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from '@nestjs/mongoose';
+import { AllExceptionsFilter } from "@microservices-demo/shared/filters";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductModule } from "./product/product.module";
+import configuration from "./config/configuration";
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+      cache: true,
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory:async (configService: ConfigService) => ({
+        uri: configService.get<string>("database.uri"),
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
